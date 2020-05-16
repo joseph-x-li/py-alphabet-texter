790a55657da2f16641cb33e160c638e26559b055
YRUDancin <13826851+joseph-x-li@users.noreply.github.com>
2020-05-15 17:14:42 -0700

    aargh

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.font import Font
 import alphabet_utils
 import matplotlib
 matplotlib.use('TkAgg')
@@ -19,23 +20,31 @@ title.grid(row=0)
 
 
 # ---------- ROW 1 ---------- matplotlib stuffs
-graph = Canvas(root, width=200, height=100, background="blue")
+graph = Canvas(root, width=380, height=100, background="blue")
 graph.create_text(50, 10, text="Hello World", font="Menlo")
 graph.grid(row=1)
 
 
 # ---------- ROW 2 ---------- to be a canvas
-title = Label(root, text="abcdefghijklmnopqrstuvwxyz", font="Menlo", fg="gray")
-title.grid(row=2, column = 0)
+alphabet_display = Text(width=51, height=1)
+alphabet_display.insert("end", "a b c d e f g h i j k l m n o p q r s t u v w x y z")
+alphabet_display.configure(state="disabled")
+
+# red_font = Font(family="Menlo", color="red", size=9)
+# gray_font = Font(family="Menlo", color="gray", size=9)
+# green_font = Font(family="Menlo", color="green", size=9)
+# alphabet_display.tag_configure("RED", red_font)
+# alphabet_display.tag_configure("GRAY", gray_font)
+# alphabet_display.tag_configure("GREEN", green_font)
+alphabet_display.grid(row=2)
 
 
 # ---------- ROW 3 ---------- 
 def on_keystroke(*args):
-    global running, prev_time, best_time
+    global running, prev_time, best_time, time_array
     running = True
-    x = au.tell(input_var.get())
-    print(x)
-    correct, letter_states, time_array = x
+    correct, letter_states, time_array = au.tell(input_var.get())
+    time_array = [(t if t >= 0 else 0) for t in time_array]
     if correct:
         running = False
         prev_time = sum(time_array)
@@ -58,10 +67,10 @@ def onReset():
 button1 = Button(root, text="Reset", font="Menlo", command=onReset)
 button1.grid(row=4)
 
-previous_time_label = Label(root, text=f"Previous Time: {'--:--' if best_time is None else 'fill'}", font="Menlo")
+previous_time_label = Label(root, text=f"Previous Time: {'-' if best_time is None else 'fill'}", font="Menlo")
 previous_time_label.grid(row=4, column = 1)
 
-best_time_label = Label(root, text=f"Best Time: {'--:--' if best_time is None else 'fill'}", font="Menlo")
+best_time_label = Label(root, text=f"Best Time: {'-' if best_time is None else 'fill'}", font="Menlo")
 best_time_label.grid(row=4, column = 2)
 
 
