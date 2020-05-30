63b6b0f45f42faa841f5c39396ec74966dec8542
YRUDancin <13826851+joseph-x-li@users.noreply.github.com>
2020-05-29 21:09:54 -0700

    PEP8 formatting

--- alphabet_graph.py
+++ alphabet_graph.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.animation import FuncAnimation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 class WidthError(Exception):
     """Raised when a list of the wrong length is passed to set_times
@@ -12,69 +14,74 @@ class WidthError(Exception):
         message -- explanation of the error
     """
 
-    def __init__(self, given, correct, message = ""):
+    def __init__(self, given, correct, message=""):
         self.message = message
         self.given = given
         self.correct = correct
         super().__init__(self.message)
 
+
 class AlphabetGraph(tk.Frame):
     FRAME_HEIGHT = 3
     X_SCALE = 5
-    
+
     def __init__(self, parent, dpi, key, *args, color="blue", ytop=0.5, interval=100, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self._parent = parent
-        
-        #private data members
+
+        # private data members
         interval = max(100, interval)
         self.key = key
         self.key_len = len(key)
         self._times = [0.0 for _ in range(self.key_len - 1)]
         self._x = list(key)[1:]
-        self._figure, self._ax = plt.subplots(figsize=(self.key_len / self.X_SCALE,
-                                                       self.FRAME_HEIGHT), 
-                                              dpi=dpi)
-        #self._ax is an matplotlib.axes.Axes object
+        self._figure, self._ax = plt.subplots(
+            figsize=(self.key_len / self.X_SCALE, self.FRAME_HEIGHT),
+            dpi=dpi)
+        # self._ax is an matplotlib.axes.Axes object
         self._canvas = FigureCanvasTkAgg(self._figure, master=self)
         self._canvas.get_tk_widget().pack(fill="both", expand=True)
-        
+
         self._barcontainer = self._ax.bar(self._x, self._times, color=color)
-        #^ is a tuple containing (patches, errorbar), where patches is a list of rectangle objects (where rectangles are artists)
-        
+        # ^ is a tuple containing (patches, errorbar), where patches is a list
+        # of rectangle objects (where rectangles are artists)
+
         self._ax.set_xlabel("Time To Press")
         self._ax.set_ylabel("Seconds")
         self._ax.set_ylim(bottom=0, top=ytop)
         self._figure.tight_layout()
-        
-        #animation, 10ms frame speed
-        self._anim = FuncAnimation(self._figure, 
-                                   self._animate, 
+
+        # animation, 10ms frame speed
+        self._anim = FuncAnimation(self._figure,
+                                   self._animate,
                                    init_func=self._init,
-                                   interval=interval, 
+                                   interval=interval,
                                    blit=True)
-    
+
     def _init(self):
         return self._animate(0)
-    
+
     def _animate(self, i):
         for rect, height in zip(self._barcontainer.patches, self._times):
             rect.set_height(height)
         return self._barcontainer.patches
-        
+
     def set_times(self, times):
         self._times = times
         return
-    
-    def reset(self):  #equivalent to setting all times to 0.0
+
+    def reset(self):
         self._times = [0.0 for _ in range(self.key_len - 1)]
         return
 
-        
+
 def main():
     root = tk.Tk()
-    AlphabetGraph(root, dpi=100, key="abpqrstuvwxyz").pack(side="top", fill="both", expand=True)
+    AlphabetGraph(root, dpi=100, key="abpqrstuvwxyz").pack(side="top",
+                                                           fill="both",
+                                                           expand=True)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
