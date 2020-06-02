803c45d871775f33df4872252bf6280e8ec04031
YRUDancin <13826851+joseph-x-li@users.noreply.github.com>
2020-06-01 21:59:00 -0700

    black formatting

--- alphabet_display.py
+++ alphabet_display.py
@@ -1,27 +1,30 @@
 import tkinter as tk
 
-class AlphabetDisplay(tk.Frame): # change to extend Text
+
+class AlphabetDisplay(tk.Frame):  # change to extend Text
     def __init__(self, parent, *args, font="Menlo", text=None, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
 
         self._display = tk.Text(self, width=51, height=1, font=font, bg="brown")
         self._display.pack(fill="both", expand=True)
         if text is None:
-            self._display.insert("end", "a b c d e f g h i j k l m n o p q r s t u v w x y z")
+            self._display.insert(
+                "end", "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+            )
         else:
             self._display.insert("end", text)
         self._display.configure(state="disabled")
         self._make_tags()
-        
+
     def reset(self):
         self.set_colors(None, 0)
-        
+
     def set_colors(self, correct, max_len):
-        #removes all tags
+        # removes all tags
         for col in ["red", "green", "black"]:
             self._display.tag_remove(col, "1.0", "1.51")
-        
-        #reapplies tags
+
+        # reapplies tags
         for x in range(26):
             start = f"1.{x * 2}"
             end = f"1.{(x * 2) + 1}"
@@ -29,19 +32,22 @@ class AlphabetDisplay(tk.Frame): # change to extend Text
                 color = "black"
             elif correct[x]:
                 color = "green"
-            else: 
+            else:
                 color = "red"
-            self._display.tag_add(color, start, end)    
-    
+            self._display.tag_add(color, start, end)
+
     def _make_tags(self):
         self._display.tag_configure("red", foreground="red")
         self._display.tag_configure("black", foreground="black")
         self._display.tag_configure("green", foreground="green")
+
 
 def main():
     root = tk.Tk()
     AlphabetDisplay(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
 
+
 if __name__ == "__main__":
-    main()        
+    main()
+
--- alphabet_utils.py
+++ alphabet_utils.py
@@ -1,5 +1,6 @@
 import time
 
+
 class AlphabetUtils:
     def __init__(self, KEY="abcdefghijklmnopqrstuvwxyz"):
         self.KEY = KEY
@@ -9,7 +10,7 @@ class AlphabetUtils:
         self.best_time = None
         self.recent_time = None
         self.times = [-1 for _ in range(self.KEYLEN)]
-        
+
     def __repr__(self):
         base = f"Previous String: {self.prev}\n"
         base += f"Current String: {self.now}\n"
@@ -17,13 +18,13 @@ class AlphabetUtils:
         base += f"Best Time: {self.best_time}\n"
         base += f"Most Recent Time: {self.recent_time}\n"
         return base
-        
+
     def _calculate_times(self):
         for i in range(self.KEYLEN):
             if i >= len(self.now):
                 self.times[i] = -1
             elif i >= len(self.prev):
-                if (self.now[i] == self.KEY[i]):
+                if self.now[i] == self.KEY[i]:
                     self.times[i] = time.time()
                 else:
                     self.times[i] = -1
@@ -33,47 +34,51 @@ class AlphabetUtils:
                 else:
                     self.times[i] = -1
         return
-    
+
     def _get_correct_chars(self):
         base = [False for _ in range(self.KEYLEN)]
         for i in range(min(len(self.now), self.KEYLEN)):
             if self.now[i] == self.KEY[i]:
                 base[i] = True
         return base
-            
+
     def _get_time_diffs(self):
-        return [(y - x) for (x, y) in zip(self.times[:(self.KEYLEN - 1)], self.times[1:])]
-    
+        return [
+            (y - x) for (x, y) in zip(self.times[: (self.KEYLEN - 1)], self.times[1:])
+        ]
+
     def tell(self, current_input):
         self.prev = self.now
         self.now = current_input
         self._calculate_times()
-        correct = (current_input == self.KEY)
+        correct = current_input == self.KEY
         time_diffs = self._get_time_diffs()
         if correct:
             self.recent_time = sum(time_diffs)
-            self.best_time = (self.recent_time if self.best_time is None else min(self.best_time, self.recent_time))
-            
-        return (correct, 
-                self._get_correct_chars(),
-                time_diffs)
-        
+            self.best_time = (
+                self.recent_time
+                if self.best_time is None
+                else min(self.best_time, self.recent_time)
+            )
+
+        return (correct, self._get_correct_chars(), time_diffs)
+
     def set_KEY(self, new_key):
         self.KEY = new_key
         self.KEYLEN = len(self.KEY)
         return
-    
+
     def reset(self):
         self.prev = ""
         self.now = ""
         self.times = [-1 for _ in range(self.KEYLEN)]
         return
-    
+
     def get_scores(self):
         x = "-" if self.recent_time is None else f"{self.recent_time:.3f}"
         y = "-" if self.best_time is None else f"{self.best_time:.3f}"
         return (x, y)
-    
+
     def reset_secores(self):
         self.recent_time = None
         self.best_time = None
